ac41e2c81882a41efe04d1bb1310dd6ea87bbfaa
Wittner Andrews <wittnerandrews@gmail.com>
2025-06-12 18:30:54 -0500

    header started, JS started over

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const readstatus = document.querySelector('.readstatus');
-const checkbox = document.querySelector('.readstatustoggle');
-checkbox.addEventListener('change', function (e){
-    e.target.checked ? readstatus.innerHTML = "Read" : readstatus.innerHTML = "Unread";
-});
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
+
+addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", "192", "yes");
+addBookToLibrary("The Hunger Games", "Suzanne Collins", "347", "no");
